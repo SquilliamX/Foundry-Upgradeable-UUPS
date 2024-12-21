9a2e7a0e97c3c8f5c7ccb0c6b029e55388048948
SquilliamX <smartchainwizz@gmail.com>
2024-12-20 21:24:48 -0500

    Update submodule URL for foundry-devops and enhance notes on upgradeable contracts; add BoxV1 and BoxV2 implementation contracts with UUPS upgradeable functionality

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Proxy is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+// first version of the implementation contract before the upgrade
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     // example
     uint256 internal number;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor // this comment turns off the warning/error of using a constructor
-    /// in the proxy.
+    /// @custom:oz-upgrades-unsafe-allow constructor // this comment turns off the warning/error of using a constructor in the proxy.
     constructor() {
         _disableInitializers(); // constructors are not used in upgradeable contracts. this is best practice to prevent initializers from happening in the constructor
     }
 
-    // intialize function is essentially a constuctor for proxies
+    // intialize function is essentially a constuctor for proxies/implementations/upgrades
     // initializer modifier makes it so that this initialize function can only be called once
     function initialize() public initializer {
         // upgradeable intializer functions should start with double underscores `__`
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Implementation is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+// second version of the implementation contract after the upgrade, this is the contract we are upgrading to
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     // example
     uint256 internal number;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor // this comment turns off the warning/error of using a constructor
-    /// in the proxy.
+    /// @custom:oz-upgrades-unsafe-allow constructor // this comment turns off the warning/error of using a constructor in the implementation.
     constructor() {
         _disableInitializers(); // constructors are not used in upgradeable contracts. this is best practice to prevent initializers from happening in the constructor
     }
 
-    // intialize function is essentially a constuctor for proxies
+    // intialize function is essentially a constuctor for proxies/implementations/upgrades
     // initializer modifier makes it so that this initialize function can only be called once
     function initialize() public initializer {
         // upgradeable intializer functions should start with double underscores `__`
@@ -24,7 +24,9 @@ contract Implementation is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         __UUPSUpgradeable_init(); // best practice to have to show this is a UUPS upgradeable contract
     }
 
-    function setNumber(uint256 _number) external { }
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
